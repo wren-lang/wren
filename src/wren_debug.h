46aaff03bcb8e5dd55a70eab7fb3aa50ef49b07a
Bob Nystrom <robert@stuffwithstuff.com>
2015-03-13 07:26:42 -0700

    Clean up debug header preamble.

--- src/wren_debug.h
+++ src/wren_debug.h
@@ -1,13 +1,5 @@
-//
-//  wren_debug.h
-//  wren
-//
-//  Created by Bob Nystrom on 11/29/13.
-//  Copyright (c) 2013 Bob Nystrom. All rights reserved.
-//
-
-#ifndef wren_wren_debug_h
-#define wren_wren_debug_h
+#ifndef wren_debug_h
+#define wren_debug_h
 
 #include "wren_value.h"
 #include "wren_vm.h"
