31d298956564b4597e115d3bc05701e380de219e
Gavin Schulz <gavin.schulz@gmail.com>
2015-03-24 23:30:53 -0700

    Remove unnecessary headers.

--- src/vm/wren_meta.h
+++ src/vm/wren_meta.h
@@ -1,12 +1,8 @@
 #ifndef wren_meta_h
 #define wren_meta_h
 
-#include <stdio.h>
-
 #include "wren.h"
 #include "wren_common.h"
-#include "wren_value.h"
-#include "wren_vm.h"
 
 // This module defines the Meta class and its associated methods.
 #if WREN_USE_LIB_META
