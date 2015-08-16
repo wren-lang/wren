6be1dce49cf9ba5758ede49f0a14173c50bbb85e
Bob Nystrom <robert@stuffwithstuff.com>
2015-08-16 09:59:53 -0700

    Link to rtlib too.

--- script/wren.mk
+++ script/wren.mk
@@ -132,7 +132,7 @@ test: $(BUILD_DIR)/test/$(WREN)
 bin/$(WREN): $(CLI_OBJECTS) $(MODULE_OBJECTS) $(VM_OBJECTS) $(LIBUV)
 	@printf "%10s %-30s %s\n" $(CC) $@ "$(C_OPTIONS)"
 	@mkdir -p bin
-	$(CC) $(CFLAGS) $^ -o $@ -lm -lpthread
+	@$(CC) $(CFLAGS) $^ -o $@ -lm -lpthread -lrt
 
 # Static library.
 lib/lib$(WREN).a: $(VM_OBJECTS)
@@ -151,7 +151,7 @@ $(BUILD_DIR)/test/$(WREN): $(TEST_OBJECTS) $(MODULE_OBJECTS) $(VM_OBJECTS) \
 		$(BUILD_DIR)/cli/io.o $(BUILD_DIR)/cli/vm.o $(LIBUV)
 	@printf "%10s %-30s %s\n" $(CC) $@ "$(C_OPTIONS)"
 	@mkdir -p $(BUILD_DIR)/test
-	$(CC) $(CFLAGS) $^ -o $@ -lm -lpthread
+	@$(CC) $(CFLAGS) $^ -o $@ -lm -lpthread -lrt
 
 # CLI object files.
 $(BUILD_DIR)/cli/%.o: src/cli/%.c $(CLI_HEADERS) $(MODULE_HEADERS) $(VM_HEADERS)
