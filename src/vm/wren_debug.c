dc2f2d9e41720568ac41528063685f11d827f6dc
scott <bonniefoo@gmail.com>
2017-01-19 22:54:25 -0500

    support for user defined state data associated with the WrenVM object

--- src/vm/wren_debug.c
+++ src/vm/wren_debug.c
@@ -10,13 +10,13 @@ void wrenDebugPrintStackTrace(WrenVM* vm)
   ObjFiber* fiber = vm->fiber;
   if (IS_STRING(fiber->error))
   {
-    vm->config.errorFn(WREN_ERROR_RUNTIME, NULL, -1, AS_CSTRING(fiber->error));
+    vm->config.errorFn(WREN_ERROR_RUNTIME, NULL, -1, AS_CSTRING(fiber->error), vm);
   }
   else
   {
     // TODO: Print something a little useful here. Maybe the name of the error's
     // class?
-    vm->config.errorFn(WREN_ERROR_RUNTIME, NULL, -1, "[error object]");
+    vm->config.errorFn(WREN_ERROR_RUNTIME, NULL, -1, "[error object]", vm);
   }
 
   for (int i = fiber->numFrames - 1; i >= 0; i--)
@@ -35,7 +35,7 @@ void wrenDebugPrintStackTrace(WrenVM* vm)
     // -1 because IP has advanced past the instruction that it just executed.
     int line = fn->debug->sourceLines.data[frame->ip - fn->code.data - 1];
     vm->config.errorFn(WREN_ERROR_STACK_TRACE, fn->module->name->value, line,
-                       fn->debug->name);
+                       fn->debug->name, vm);
   }
 }
 
