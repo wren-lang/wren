049b115d01f38f9bb98781e2aaacc93cb492ac1e
jclc <jclc@protonmail.com>
2018-04-30 04:39:46 +0300

    fix build_libuv.py shebang

--- util/build_libuv.py
+++ util/build_libuv.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Runs GYP to generate the right project then uses that to build libuv.
+
+from __future__ import print_function
 
 import os
 import platform
