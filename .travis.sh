8e29b0e4ee6704c0e8d1a231b82beb9211909ee8
underscorediscovery <sven@underscorediscovery.com>
2020-05-28 09:29:51 -0700

    0.3.0; more updating for travis

---
+++ .travis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# This build script only builds mac or linux right now, for CI.
+WREN_WD="projects/gmake"
+if [ -n "$WREN_TARGET_MAC" ]
+then
+  WREN_WD="projects/gmake.mac"
+fi
+
+WREN_PY="python3"
+if [ -n "$WREN_PY_BINARY" ]
+then
+  WREN_PY="$WREN_PY_BINARY"
+fi
+
+echo "using working directory '$WREN_WD' ..."
+echo "using python binary '$WREN_PY' ..."
+
+cd "$WREN_WD" && make config=debug_64bit-no-nan-tagging
+cd ../../ && $WREN_PY ./util/test.py --suffix=_d
+
+cd "$WREN_WD" && make config=debug_64bit
+cd ../../ && $WREN_PY ./util/test.py --suffix=_d
+
+cd "$WREN_WD" && make config=release_64bit-no-nan-tagging
+cd ../../ && $WREN_PY ./util/test.py
+
+cd "$WREN_WD" && make config=release_64bit
+cd ../../ && $WREN_PY ./util/test.py
