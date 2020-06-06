a825083bd19e158b9ca4ea9d9873f8cf2c614d0f
Dario Vladovic <d.vladimyr@gmail.com>
2020-06-06 23:35:10 +0200

    Refactor travis script

--- .travis.sh
+++ .travis.sh
@@ -3,28 +3,23 @@ set -e
 
 # This build script only builds mac or linux right now, for CI.
 WREN_WD="projects/make"
-if [ -n "$WREN_TARGET_MAC" ]
-then
+if [ -n "$WREN_TARGET_MAC" ]; then
   WREN_WD="projects/make.mac"
 fi
 
-WREN_PY="python3"
-if [ -n "$WREN_PY_BINARY" ]
-then
-  WREN_PY="$WREN_PY_BINARY"
-fi
+WREN_PY=${WREN_PY_BINARY:-python3}
 
 echo "using working directory '$WREN_WD' ..."
 echo "using python binary '$WREN_PY' ..."
 
-cd "$WREN_WD" && make config=debug_64bit-no-nan-tagging
-cd ../../ && $WREN_PY ./util/test.py --suffix=_d
+make -C $WREN_WD config=debug_64bit-no-nan-tagging
+$WREN_PY ./util/test.py --suffix=_d
 
-cd "$WREN_WD" && make config=debug_64bit
-cd ../../ && $WREN_PY ./util/test.py --suffix=_d
+make -C $WREN_WD config=debug_64bit
+$WREN_PY ./util/test.py --suffix=_d
 
-cd "$WREN_WD" && make config=release_64bit-no-nan-tagging
-cd ../../ && $WREN_PY ./util/test.py
+make -C $WREN_WD config=release_64bit-no-nan-tagging
+$WREN_PY ./util/test.py
 
-cd "$WREN_WD" && make config=release_64bit
-cd ../../ && $WREN_PY ./util/test.py
+make -C $WREN_WD config=release_64bit
+$WREN_PY ./util/test.py
