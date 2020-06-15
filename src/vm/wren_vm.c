3676a5b727e1dbb4a4b2a1a94b99cdb252350197
Michel Hermier <michel.hermier@gmail.com>
2020-06-15 13:37:44 +0200

    wren/vm: Allow wrenInterpret to call foreign function (complement 344d343 at fixing #730).

--- src/vm/wren_vm.c
+++ src/vm/wren_vm.c
@@ -1447,12 +1447,9 @@ WrenInterpretResult wrenInterpret(WrenVM* vm, const char* module,
   wrenPushRoot(vm, (Obj*)closure);
   ObjFiber* fiber = wrenNewFiber(vm, closure);
   wrenPopRoot(vm); // closure.
-
-  WrenInterpretResult result = runInterpreter(vm, fiber);
-
-  vm->fiber = NULL;
   vm->apiStack = NULL;
-  return result;
+
+  return runInterpreter(vm, fiber);
 }
 
 ObjClosure* wrenCompileSource(WrenVM* vm, const char* module, const char* source,
