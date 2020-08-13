a253212f067af114ab6d8f26c7e17e3c548376bb
Chayim Refael Friedman <chayimfr@gmail.com>
2020-08-13 09:27:48 +0300

    Fix test/benchmark/fib.py

--- test/benchmark/fib.py
+++ test/benchmark/fib.py
@@ -6,7 +6,7 @@ def fib(n):
   if n < 2: return n
   return fib(n - 1) + fib(n - 2)
 
-start = time.clock()
+start = time.perf_counter()
 for i in range(0, 5):
   print(fib(28))
-print("elapsed: " + str(time.clock() - start))
+print("elapsed: " + str(time.perf_counter() - start))
