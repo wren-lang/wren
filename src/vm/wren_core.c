245d94acc019a4c33e15a3c2f5cdf006d2506b3c
Vladimir Serbinenko <phcoder@gmail.com>
2022-05-29 12:30:02 +0200

    Resync fmax-fmin with musl implementation

--- src/vm/wren_core.c
+++ src/vm/wren_core.c
@@ -14,13 +14,17 @@
 #include "wren_core.wren.inc"
 
 #ifdef __DJGPP__
+/* fmax/fmin copied from musl which is distributed under MIT license.  */
 double fmax(double x, double y)
 {
   if (isnan(x))
     return y;
   if (isnan(y))
     return x;
-  return x > y ? x : y;
+  /* handle signed zeros, see C99 Annex F.9.9.2 */
+  if (signbit(x) != signbit(y))
+    return signbit(x) ? y : x;
+  return x < y ? y : x;
 }
 
 double fmin(double x, double y)
@@ -29,6 +33,9 @@ double fmin(double x, double y)
     return y;
   if (isnan(y))
     return x;
+  /* handle signed zeros, see C99 Annex F.9.9.2 */
+  if (signbit(x) != signbit(y))
+    return signbit(x) ? x : y;
   return x < y ? x : y;
 }
 #endif
